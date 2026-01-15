2fce95757a9a8a2b5f4dcbf7d4f86153180aa07f
Priyanshi   createscode <choudharypriyanshi667@gmail.com>
2026-01-15 23:00:27 +0530

    Delete Basic program/AveragePCM.java

--- Basic program/AveragePCM.java
+++
@@ -1,9 +0,0 @@
-public class AveragePCM{
-   public static void main(String[] args){
-   int Mathsmarks=94;
-   int Physicsmarks=95;
-   int Chemistrymarks=96;
-   double Average=(Mathsmarks+ Physicsmarks+Chemistrymarks)/3.0;
-   System.out.print("Sam Average marks in PCM is "+Average);
-   }
-   }
