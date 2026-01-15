1e8e03ac6f5609eabab9caf5de4c6f40aab52bfe
priyanshiglacs25-alt <priyanshi.gla_cs25@gla.ac.in>
2026-01-15 23:56:38 +0530

    assignment-2 level-1 problem-8

---
+++ basic program/KmToMilesInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class KmToMilesInput{
+     public static void main(String[] args){
+	 Scanner sc = new Scanner(System.in);
+	 System.out.print("Enter km:");
+	 double km=sc.nextDouble();
+	 double miles=km*1.6;
+	 System.out.println("The total miles is"+miles+"for given km"+km);
+	 }
+	 }
