2771b72a326e1e2014eb08d79e202910019ea6dd
priyanshiglacs25-alt <priyanshi.gla_cs25@gla.ac.in>
2026-01-22 22:04:04 +0530

    Add Arguments.java only

---
+++ Arguments.java
@@ -0,0 +1,10 @@
+public class Arguments{
+    public static void main(String[] args){
+	System.out.println(args[0]);
+	System.out.println(args[1]);
+	System.out.println(args[0] + args[1]);
+	System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+	System.out.println(args[0].charAt(0) + args[1].charAt(1));
+	
+	}
+	}
