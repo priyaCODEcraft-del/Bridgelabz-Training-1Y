c507b0de977a49e8d61c217fff4e6d590eb4e652
priyanshiglacs25-alt <priyanshi.gla_cs25@gla.ac.in>
2026-01-22 07:28:02 +0530

    assignmnent-3 level-1 program-6

---
+++ basic program/PositiveNegative.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+   public class PositiveNegative{
+   public static void main(String[] args){
+   Scanner input = new Scanner(System.in);
+   System.out.println("enter number=");
+   input number = input.nextInt();
+   if(number>0){
+   System.out.println("Positive");
+   }
+   else if(number==0){
+   System.out.println("zero");
+   }else{
+   System.out.println("Negative");
+}
+}
+}
