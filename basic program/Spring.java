bed82c70794ff7e5b4811837324d9d1c67af6c1e
priyanshiglacs25-alt <priyanshi.gla_cs25@gla.ac.in>
2026-01-24 14:35:17 +0530

    assignment-3 level-1 problem-7

---
+++ basic program/Spring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Spring{
+    public static void main(String[] args){
+	Scanner input =new Scanner(System.in);
+	System.out.println("enter the month number:");
+	int month = input.nextInt();
+	System.out.println("enter the day");
+	int day= input.nextInt();
+	if ((month==3 &&day>=20) ||(month==4 )||(month==5 )||(month==6 &&day<=20)){
+	System.out.println("Its a Spring Season");
+	}else{ 
+	System.out.println("Not a Spring Season");
+	}
+	}
+}
