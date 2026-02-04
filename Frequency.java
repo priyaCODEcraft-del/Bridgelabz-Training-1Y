e04dfb8e5e029da446060751dc8eb223b88c7994
priyanshiglacs25-alt <priyanshi.gla_cs25@gla.ac.in>
2026-02-04 21:27:14 +0530

    Assignment-4 level-2 problem-10

---
+++ Frequency.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Frequency{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] freq = new int[10];
+
+        while (num > 0) {
+            int digit = num % 10;
+            freq[digit]++;
+            num /= 10;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > 0)
+                System.out.println(i + " → " + freq[i]);
+        }
+    }
+}
